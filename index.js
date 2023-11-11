1e7b24696ba420333503a06352f307a9e1f5c4b5
Artemijs Matusev <matu6ev@gmail.com>
2023-11-11 18:45:34 +0000

    Added styling to home page, set up alert messages

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ const port = 3000;
 //Adding static files and middleware
 app.use(express.static("public"));
 
+//Colors for Alert messages
+const green = "#149886";
+const red = "#A45D5D"
+
+
+// ----- FUNCTIONS -----
 
 
 // ------ END POINTS --------
 app.get("/", (req, res) => {
     res.render("index.ejs");
+})
+
+app.post("/register", (req, res) => {
+    res.render("index.ejs", {alertMessage: "Account has been created", alertColor: green})
 })
 
 
